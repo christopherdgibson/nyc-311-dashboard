49ec132fbabc8457c72ce6348ba2c8b0bb00505f
christopherdgibson <christopherdgibson@users.noreply.github.com>
2026-04-03 19:19:30 +0000

    deploy: 81fb7a3b64d21cef771e82a3730e2a40a2a51bed

---
+++ js/custom-javascript.js
@@ -0,0 +1,221 @@
+﻿function scrollToTop(behavior = "smooth") {
+    window.scrollTo({ top: 0, behavior: behavior });
+}
+
+function changeClassName(classNameOld, classNameNew) {
+    const el = document.querySelector(`.${classNameOld}`);
+    if (el) {
+        el.className = classNameNew;
+    }
+}
+
+function toggleClassName(element, className) {
+    document.querySelector(element).classList.toggle(className);
+}
+
+//function closeNavOnClick() { // when sidebar toggled nav with class
+//    document.querySelector('nav ul').addEventListener('click', function (e) {
+//        if (e.target.tagName === 'A') {
+//            this.classList.remove('nav-open');
+//        }
+//    });
+//}
+
+window.setIndeterminateSelection = (element, indeterminate) => {
+    if (element) {
+        element.indeterminate = indeterminate;
+    }
+};
+
+function closeNavOnClick() {
+    document.querySelectorAll('nav ul a').forEach(function (link) {
+        link.addEventListener('click', () => {
+            document.getElementById('checkNav').checked = false;
+        });
+    });
+}
+
+window.closeDropdownOnClickAway = (dotNetRef) => {
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.sidebar-dropdown-stack')) {
+            dotNetRef.invokeMethodAsync('OnGlobalClick');
+        }
+    });
+};
+
+window.saveAsFile = function (fileName, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = fileName;
+    link.href = "data:application/pdf;base64," + bytesBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+window.saveElementAsPdf = function (elementId, options) {
+    console.log("options:", options);
+    console.log("options.image:", options.image);
+    var element = document.getElementById(elementId);
+    var opt = {
+        margin: options.margin,
+        filename: options.fileName,
+        image: {
+            type: options.image.type,
+            quality: options.image.quality
+        },
+        html2canvas: {
+            scale: options.html2canvas.scale
+        },
+        jsPDF: {
+            unit: options.jsPDF.unit,
+            format: options.jsPDF.format,
+            orientation: options.jsPDF.orientation
+        },
+        pagebreak: {
+            mode: options.pagebreak.mode,
+            avoid: options.pagebreak.avoid
+        }
+    };
+    html2pdf().set(opt).from(element).save();
+}
+
+window.renderApexChart = function (elementSelector, dataset, options) {
+    function applyFormat(formatStr, val) {
+        if (!formatStr) return val;
+        if (formatStr === "integer") return val.toFixed(0);
+        if (formatStr.startsWith("decimal:")) {
+            const places = parseInt(formatStr.split(":")[1], 10);
+            return val.toFixed(places);
+        }
+        // Extend here if needed
+        return val;
+    }
+
+    function applyFormatter(formatters, defaultFormatter, seriesIndex, val) {
+        const fallback = defaultFormatter ?? formatters?.[0];
+        const fmt = formatters?.[seriesIndex] ?? fallback;
+        return applyFormat(fmt, val);
+    }
+
+    var chartOptions = {
+        chart: {
+            type: options.chart.type,
+            height: options.height,
+            width: options.width
+        },
+        series: dataset.series,
+        xaxis: {
+            categories: dataset.categories
+        }
+    };
+
+    if (options.xaxis) {
+        chartOptions.xaxis.labels = {
+            rotate: options.xaxis.labels.rotate,
+            hideOverlappingLabels: options.xaxis.labels.hideOverlappingLabels,
+            trim: options.xaxis.labels.trim,
+            maxHeight: options.xaxis.labels.maxHeight
+        };
+    }
+
+    if (options.dataLabels) {
+        chartOptions.dataLabels = {
+            enabled: options.dataLabels.enabled,
+            formatter: function (val, { seriesIndex }) {
+                return applyFormatter(options.dataLabels.seriesFormatters.formatters, options.dataLabels.seriesFormatters.fallback, seriesIndex, val);
+            },
+            offsetY: options.dataLabels.offsetY,
+            style: {
+                fontSize: options.dataLabels.style.fontSize,
+                colors: options.dataLabels.style.colors
+            }
+        };
+    }
+
+    if (options.plotOptions) {
+        chartOptions.plotOptions = {
+            bar: {
+                borderRadius: options.plotOptions.bar.borderRadius,
+                dataLabels: { position: options.plotOptions.bar.dataLabelsPosition }
+            }
+        };
+    }
+
+    if (options.tooltip) {
+        chartOptions.tooltip = {
+            enabled: options.tooltip.enabled,
+            y: {
+                formatter: function (val, { seriesIndex }) {
+                    return applyFormatter(options.tooltip.seriesFormatters.formatters, options.tooltip.seriesFormatters.fallback, seriesIndex, val);
+                }
+            }
+        };
+    }
+
+    if (options.yaxis) {
+        chartOptions.yaxis = {
+            labels: {
+                formatter: function (val) {
+                    return applyFormat(options.yaxis.labelsFormatter, val);
+                }
+            }
+        };
+    }
+
+    window.chartInstances = window.chartInstances || {};
+
+    var chartDiv = document.querySelector(elementSelector);
+    if (chartDiv) {
+        // destroy existing instance if re-rendering
+        if (window.chartInstances[elementSelector]) {
+            window.chartInstances[elementSelector].destroy();
+        }
+        window.chartInstances[elementSelector] = new ApexCharts(chartDiv, chartOptions);
+        window.chartInstances[elementSelector].render();
+        return null;
+    } else {
+        return `Chart element '${elementSelector}' not found`;
+    }
+};
+
+window.updateApexChart = function (options) {
+    var chartDiv = document.getElementById("chart");
+    console.log("updateApexChart called. Instance:", window.myApexChartInstance, "Div:", chartDiv, "Options:", options);
+    if (window.myApexChartInstance && chartDiv) {
+        console.log("Chart instance found ");
+        //console.log("Calling updateOptions with:", options);
+        //window.myApexChartInstance.updateOptions(options, true, true, true);
+        console.log("Calling updateOptions with:", options.xaxis.categories);
+        //window.myApexChartInstance.updateOptions({
+        //    xaxis: { categories: options.xaxis.categories }
+        // ...other options...
+        //}, true, true, true);
+        console.log("Calling updateSeries with:", options.series);
+        window.myApexChartInstance.updateSeries(options.series, true);
+    } else if (chartDiv) {
+        console.log("Create new chart ");
+        window.myApexChartInstance = new ApexCharts(chartDiv, options);
+        window.myApexChartInstance.render();
+    } else {
+        console.error("Chart div not found!");
+    }
+};
+
+window.notifyDotNetAfterChartDiv = function (dotNetRef) {
+    setTimeout(() => {
+        dotNetRef.invokeMethodAsync('OnChartDivReady');
+    }, 0); // Or use MutationObserver?
+};
+
+window.notifyChartDivReady = function (dotNetHelper) {
+    setTimeout(function () {
+        dotNetHelper.invokeMethodAsync('RenderChartFromJS');
+    }, 0);
+}
+
+window.disposeApexChart = function () {
+    if (window.myApexChartInstance) {
+        window.myApexChartInstance.destroy();
+        window.myApexChartInstance = null;
+    }
+}
---
+++ service-worker-assets.js
@@ -0,0 +1,273 @@
+self.assetsManifest = {
+  "version": "r1utVSpY",
+  "assets": [
+    {
+      "hash": "sha256-8ZRc1sGeVrPBx4lD717BgRaQekyh78QKV9SKsdt638U=",
+      "url": ".nojeykyll"
+    },
+    {
+      "hash": "sha256-vPO8i9x13Rhbpffj5Y+TlLwFb8uhp29yTjNQq2Ew+WA=",
+      "url": "404.html"
+    },
+    {
+      "hash": "sha256-dcl/h7JPKeeQh0ZxUlhcQ8d6D95cZ5mvkWV7kyl8QSc=",
+      "url": "NYC311Dashboard.styles.css"
+    },
+    {
+      "hash": "sha256-9Wz0tEAcx/822bVOO4xZJ6mn8kK3bZncDYv9WXUKwCc=",
+      "url": "_framework/CSharpFunctionalExtensions.wasm"
+    },
+    {
+      "hash": "sha256-+KSCUhpVCXHROpRUJH9wzQ+raVNlFcApZ04W9aRpZi8=",
+      "url": "_framework/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-x1F82njgsF9dzQZyrJ0ztCzBd2r766MNbpIjisdGtB0=",
+      "url": "_framework/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-h5r0gsHK6ByjGWzd8JuuIyxEatn97r+IWQz1oBrL+Zw=",
+      "url": "_framework/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-XR+WUfg8LtnrwdXFBnV+qO3ROyp6cYCfl1AZajIAheM=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-Nbv4DDWJLbUri+9QrX+3vnA2FV/Qux8Jm1buTARMlBs=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-od574rRtVVRJj+24jbl8j3GdQFpxlJOpSx+pEMw1Q18=",
+      "url": "_framework/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-irBjHyfXyj/e/6tL02UcVrYxZ4Q4H6KqBJQb3ce18pk=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-gPl1KWGxLIxxQgQ9BG1m/niMzqZGG651efBdnDY3kDc=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-ssFNY2Ai861hrwT+m7wbuHPLX6nsTeY/M/fevNYMQl8=",
+      "url": "_framework/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-KCdgvZvpkT0QTlCjj/PtlVlJnNw1mDlJAtaRSWXz0YM=",
+      "url": "_framework/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-cchAFw7i1bV5sLP4di8m2zT/hk9nMqHGh5hmUgXyAtQ=",
+      "url": "_framework/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-xyqqv0SMCahnrOs+p5QYDHoh/BAYlbNtyjTTpqmRW/g=",
+      "url": "_framework/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-73fobmdfDEgPi5c9HIlVzjHZtWrtF7HElTQFBTEttnQ=",
+      "url": "_framework/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-xhOzknjnlp+hIaCO10hzOPMPfN1m23PsjwQRP5XThwU=",
+      "url": "_framework/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-j6wYgyZCZztTLaq6d8afP6XW4fnwBPgMtN/jzLYXI5g=",
+      "url": "_framework/NYC311Dashboard.wasm"
+    },
+    {
+      "hash": "sha256-J9fWKhcDnAzzSEv05fkPwrG1HzvLEsumMjlg/nBSW0U=",
+      "url": "_framework/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-x1I5yIydjBMhdtLJeeMIUfInE8i7y6F8m/3BDI+9/3k=",
+      "url": "_framework/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-l3btxU7skX3nqOn358T4gGvW7ZU/HNd0wkp4j6t8DWs=",
+      "url": "_framework/System.ComponentModel.Annotations.wasm"
+    },
+    {
+      "hash": "sha256-F2qjYBXWxtewpm29FIk7CosIp5viFHegk54GkyQcEQ0=",
+      "url": "_framework/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-qHQyJccmDLuze9bbCQB6P8FUd0fmw2eDuwJ42Nep4mg=",
+      "url": "_framework/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-u6gbLKyx6hoicimJF2vYcDYS65XGTHvyWsU0kHREt/s=",
+      "url": "_framework/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-Rv3s5cr39eq6ZbhNVYKRaF1tYTXL7f39utQJlOuqVt4=",
+      "url": "_framework/System.Linq.Expressions.wasm"
+    },
+    {
+      "hash": "sha256-QBic4phlTEa6Kz8wnXSjUv/NfMCt+mLnw2lryGj9IwE=",
+      "url": "_framework/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-TLAWSJHCPW3N+zTdWKuQP6jZrmK2z/i3RI8Ys+iwC/c=",
+      "url": "_framework/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-pw1HVJKBSTZZt8GgGlnCUhLgxVyiJ4AxY5i78FL5V3o=",
+      "url": "_framework/System.Net.Http.Json.wasm"
+    },
+    {
+      "hash": "sha256-0V8vfjzG5qSv/fBJXe30CoaeRYtuG8LiQrcjnL/GES4=",
+      "url": "_framework/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-dSBa23PLlzOUa3eHxDj1nzYQzf95XnmQ/VECEmmnMXw=",
+      "url": "_framework/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-ZNVr5JPemN0b9BU5sbZLJVNVPIUlcc2ReHr9oCdYEWA=",
+      "url": "_framework/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-jhqjByf67YCTJsiG1pud8m+gkKGa3lK9MW6N/2Sy0Sc=",
+      "url": "_framework/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-a7OLC5RpQy7RT6aQZ22rTXIPWtmdPedaiZgQRFdujxU=",
+      "url": "_framework/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-0EdoNH7lD/T+WOkCyPTQqU1NqxXXXhi4Ph8EgvDy21A=",
+      "url": "_framework/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-xxR9gvJvgSJPKKoV4MAg3dH8lg2H5bb8ajjdxwdxvZA=",
+      "url": "_framework/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-LrBuXyTSVfzvSSKMZv8RVfuSlztQp8PoIQpoIizl76o=",
+      "url": "_framework/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-6wENzjxLtMYx+ZlD7Q/NA8Fct/sHaGd3QWpS7Qt5sXQ=",
+      "url": "_framework/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-ENhd1y6fPEWCHgJ+30QofS2XcgQPp/iZ165IOL19b+A=",
+      "url": "_framework/System.Threading.wasm"
+    },
+    {
+      "hash": "sha256-G8+cDjUW3GN0xd7KCY2Pv31yrsJ0IUR4k3hhhC005sI=",
+      "url": "_framework/System.Transactions.Local.wasm"
+    },
+    {
+      "hash": "sha256-wg7h7C/xX+Y6AdHsscNC4M9udqhuz2CiSfor6DN4KHk=",
+      "url": "_framework/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-gvHfnndfEu1tRf0rFb5988rWq7ITIotOaE8+AMbKYbc=",
+      "url": "_framework/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-bUzhz0OnegEsZmylsTKgDymH/pcF8PJt7ZuZE9B9wyE=",
+      "url": "_framework/dotnet.js"
+    },
+    {
+      "hash": "sha256-A16ka545FHaB+QXjyJ4exXh7XpBAAIFnX8YkC4xAng8=",
+      "url": "_framework/dotnet.native.js"
+    },
+    {
+      "hash": "sha256-s6EBTPv470xCV3v7vg6YPKgsWog8JDbzEWXbT6KucuQ=",
+      "url": "_framework/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-sRdYoaUiZmrqOtPGSBpgxZxxSQsILEw5kzx7dL4zlfw=",
+      "url": "_framework/dotnet.runtime.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-AEMichyFVzMXWbxt2qy7aJsPBxXWiK7IK9BW0tW1zDs=",
+      "url": "css/bootstrap/bootstrap-icons.css"
+    },
+    {
+      "hash": "sha256-pdY4ejLKO67E0CM2tbPtq1DJ3VGDVVdqAR6j3ZwdiE4=",
+      "url": "css/bootstrap/bootstrap-icons.min.css"
+    },
+    {
+      "hash": "sha256-z8OR40MowJ8GgK6P89Y+hiJK5+cclzFHzLhFQLL92bg=",
+      "url": "css/bootstrap/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-gBwg2tmA0Ci2u54gMF1jNCVku6vznarkLS6D76htNNQ=",
+      "url": "css/bootstrap/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-9VUTt7WRy4SjuH/w406iTUgx1v7cIuVLkRymS1tUShU=",
+      "url": "css/bootstrap/fonts/bootstrap-icons.woff"
+    },
+    {
+      "hash": "sha256-bHVxA2ShylYEJncW9tKJl7JjGf2weM8R4LQqtm/y6mE=",
+      "url": "css/bootstrap/fonts/bootstrap-icons.woff2"
+    },
+    {
+      "hash": "sha256-W/sfnfsFGiwLrRCA7kmdEvVzcoBXBw05D1Dfp4GRtIU=",
+      "url": "css/styles.css"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-LGGQW9fnpSYNMCo38yZRqRFxHWx3wk6W5NQgMfGUV2c=",
+      "url": "images/NYC_311_logo.png"
+    },
+    {
+      "hash": "sha256-CpsTUwIOSAjMTWiUvLKH3UQmc75lvZAKLplpUZ8Olbk=",
+      "url": "images/loading.gif"
+    },
+    {
+      "hash": "sha256-DlSuNK9jINBdxCpqpxMIhHosGZ1MYrO1SRrLBOkycbg=",
+      "url": "images/nyc_background.png"
+    },
+    {
+      "hash": "sha256-3Nrvj3SGkpT4Qzceh63nXIbGsQHaf3oc0pyX2aZ1lns=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-syuJjB0G9nL1PobIgLU1U+4k5U6FKTKL8Z0vnoU6aEs=",
+      "url": "js/custom-javascript.js"
+    },
+    {
+      "hash": "sha256-C4dAz89769OFv+kTj24792F+4sd1XrKLLhJ3ZzQsfDY=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-0xzrauo56MygteZee67eroR1CMAhw+3HzMkM7deQGVo=",
+      "url": "sample-data/NYC311Requests.json"
+    },
+    {
+      "hash": "sha256-L8PtGeVGnRepM2V5YN3kKp41y50jUnNrM3+HF1lBruc=",
+      "url": "sample-data/NYC311Requests20260228.json"
+    },
+    {
+      "hash": "sha256-EzyDt4kZL1DVrgDvxHJsyMIkuqsXxsRuAi4kW+/Xhjc=",
+      "url": "sample-data/NYC311RequestsAbbr.json"
+    }
+  ]
+};
